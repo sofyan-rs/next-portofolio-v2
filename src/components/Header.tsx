29d6795bd04a432ebe7196924402e8f54ddade32
Sofyan R <sofyan423@gmail.com>
2023-06-03 13:20:19 +0700

    fix initial toggle icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,9 @@ import { useEffect, useState } from "react";
 const Header = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
+  const storedTheme = localStorage.getItem("theme") || "light";
   const [toggleIcon, setToggleIcon] = useState(
-    currentTheme === "light" ? faMoon : faSun
+    storedTheme === "dark" ? faSun : faMoon
   );
 
   const menus = [
@@ -36,10 +37,10 @@ const Header = () => {
   ];
 
   useEffect(() => {
-    if (currentTheme === "light") {
-      setToggleIcon(faMoon);
-    } else {
+    if (currentTheme === "dark") {
       setToggleIcon(faSun);
+    } else {
+      setToggleIcon(faMoon);
     }
   }, [currentTheme]);
 
@@ -50,17 +51,16 @@ const Header = () => {
           <FontAwesomeIcon icon={faCarBattery} fixedWidth />
         </div>
         <div className="flex items-center gap-7">
-          <ul className="left-0 right-0 bottom-0 bg-white p-3 border-t-2 border-gray-100 text-xs shadow-lg text-center fixed z-50 w-full grid grid-cols-4 gap-2 font-bold md:relative md:flex md:text-base md:bg-transparent md:shadow-none md:border-none md:gap-7">
+          <ul className="left-0 right-0 bottom-0 bg-white p-3 border-t-2 border-gray-100 text-xs shadow-lg text-center fixed z-50 w-full grid grid-cols-4 gap-2 font-bold md:relative md:flex md:text-base md:bg-transparent md:shadow-none md:border-none md:gap-7 dark:bg-slate-900 dark:border-slate-700">
             {menus.map((menu) => (
               <li key={menu.id}>
                 <button onClick={() => scrollElementToIdMinusHeight(menu.id, 68)} className="menu hover:text-red-400 transition flex flex-col items-center w-full">
-                  <FontAwesomeIcon icon={menu.icon} fixedWidth className="icon text-lg text-gray-400 px-5 py-1.5 rounded-sm mb-1 transition md:hidden" />
+                  <FontAwesomeIcon icon={menu.icon} fixedWidth className="icon text-lg text-slate-400 px-5 py-1.5 rounded-sm mb-1 transition md:!hidden" />
                   <span className="block">{menu.label}</span>
                 </button>
               </li>
             ))}
           </ul>
-          {/* The current theme is: {theme} */}
           <button onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")} className="text-red-400 bg-gray-100 p-1 text-xl rounded-sm dark:bg-slate-800">
           <FontAwesomeIcon icon={toggleIcon} fixedWidth />
           </button>
