fdbf623e1ce48883fed6359777558a73241d597c
Sofyan R <sofyan423@gmail.com>
2023-10-09 01:31:28 +0700

    fix modal overflow

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -17,14 +17,16 @@ const Modal = ({ showModal, handleShow, title, children }: Props) => {
           showModal ? "top-[40px]" : "-top-[4000px]"
         } fixed left-0 right-0 z-50 mx-auto flex w-[500px] max-w-full place-items-center justify-center overflow-hidden transition-all ease-out`}
       >
-        <div className="mx-4 max-h-[calc(100vh-70px)] w-full overflow-auto rounded-sm bg-white dark:bg-slate-700">
+        <div className="mx-4 w-full rounded-sm bg-white dark:bg-slate-700">
           <div className="border-gray-4 relative flex justify-between gap-3 border-b border-slate-100 px-5 py-4 dark:border-slate-600">
             <span className="font-semibold">{title}</span>
             <button className="text-xl text-red-400" onClick={handleShow}>
-            <XIcon />
+              <XIcon />
             </button>
           </div>
-          {children}
+          <div className="max-h-[calc(100vh-140px)] overflow-auto">
+            {children}
+          </div>
         </div>
       </div>
     </>
